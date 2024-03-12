189c65f63299aa8c2cb3ae81fa3ed8a33a7c9faa
RPierini <rodrix10@gmail.com>
2024-03-12 22:51:25 +0000

    adicionando opcao de argumentos e adicionando alguns printf para debug

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ CFLAGS=-march=native -mtune=native -O3 -fomit-frame-pointer -fwrapv
 
 FORRO_NEON_SRC=src/forro/arm-neon/forro.c
 FORRO_REF_SRC=src/forro/ref/forro.c
+FORRO_DEBUG_SRC=src/forro/ref/forro-debug.c
+FORRO_ARG_SRC=src/forro/forro-args.c
 FORRO_X86_SRC=src/forro/x86-simd/forro.c
 XOTE_REF_SRC=src/xote/ref/xote.c
 XOTE_X86_SRC=src/xote/x86-simd/xote.c
@@ -26,6 +28,8 @@ $(BUILD_DIR)/test_neon: neon
 
 test_ref: $(BUILD_DIR)/test_ref
 ref: $(BUILD_DIR)/xote_ref.o $(BUILD_DIR)/forro_ref.o
+ref-args: $(BUILD_DIR)/forro_debug.o $(BUILD_DIR)/forro-args
+
 
 $(BUILD_DIR)/xote_ref.o:
 	@mkdir -p $(BUILD_DIR)
@@ -34,6 +38,14 @@ $(BUILD_DIR)/xote_ref.o:
 $(BUILD_DIR)/forro_ref.o:
 	@mkdir -p $(BUILD_DIR)
 	@$(CC) $(CFLAGS) -c $(FORRO_REF_SRC) -o $@
+
+$(BUILD_DIR)/forro_debug.o:
+	@mkdir -p $(BUILD_DIR)
+	@$(CC) $(CFLAGS) -c $(FORRO_DEBUG_SRC) -o $@
+
+$(BUILD_DIR)/forro-args:
+	@mkdir -p $(BUILD_DIR)
+	@$(CC) $(CFLAGS) -I. $(BUILD_DIR)/forro_debug.o $(FORRO_ARG_SRC) -o $@
 
 test_x86_simd: $(BUILD_DIR)/test_x86_simd
 x86_simd: $(BUILD_DIR)/xote_x86_simd.o $(BUILD_DIR)/forro_x86_simd.o
---
+++ src/forro/forro-args.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "src/forro/ref/forro-debug.h"
+
+int main(int argc, char *argv[])
+{
+    // criando contexto e saida
+    stream_ctx input;
+    uint64_t number_of_bytes = 128;
+    uint8_t *output_forro = (uint8_t *)malloc(number_of_bytes);
+    uint8_t *mensagem = (uint8_t *)malloc(number_of_bytes);
+    uint8_t *cifrado = (uint8_t *)malloc(number_of_bytes);
+
+    // capturando chave do primeiro argumento
+    uint8_t key[32];
+    strncpy(key, argv[1], 32);
+
+    // capturando nonce do segundo argumento
+    uint8_t iv[8];
+    strncpy(iv, argv[2], 8);
+
+    // capturando mensagem a ser cifrada
+    strncpy(mensagem, argv[3], 128);
+
+    // carregando chave, iv e rodando o algoritmo
+    forro_keysetup(&input, key);
+    forro_ivsetup(&input, iv);
+    forro_encrypt_bytes(&input, mensagem, cifrado, number_of_bytes);
+    printf("\n");
+    forro_keystream_bytes(&input, output_forro, number_of_bytes);
+
+    // imprimindo mensagem
+    printf("Mensagem:\n");
+    for (int i = 0; i < 128; i++)
+    {
+        printf("%02x", mensagem[i]);
+        if (i % 16 == 15)
+            printf("\n");
+    }
+    printf("\n");
+
+    // imprimindo detalhes da chave e do Nonce
+    printf("KEY:\n");
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%02x", key[i]);
+        if (i % 16 == 15)
+            printf("\n");
+    }
+    printf("\nNONCE:\n");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%02x", iv[i]);
+        if (i % 8 == 7)
+            printf("\n");
+    }
+
+    // imprimindo resultado em bloco e depois em texto corrido
+    printf("\nResultado:\n");
+    for (int i = 0; i < 8; i++) {
+        for (int k = 0; k < 16; k++) {
+            printf("%02x", output_forro[16 * i + k]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 8; i++) {
+        for (int k = 0; k < 16; k++) {
+            printf("%02x", output_forro[16 * i + k]);
+        }
+    }
+    printf("\n");
+
+    printf("\nCifrado:\n");
+    for (int i = 0; i < 8; i++) {
+        for (int k = 0; k < 16; k++) {
+            printf("%02x", cifrado[16 * i + k]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 8; i++) {
+        for (int k = 0; k < 16; k++) {
+            printf("%02x", cifrado[16 * i + k]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
